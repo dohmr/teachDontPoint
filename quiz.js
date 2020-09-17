aa3836336a6b0faef7c28696cc4037a060651a05
doh <theshawns0n@gmail.com>
2020-09-17 00:45:28 -0400

    Added form for HS. Css. Timer. Points not set

--- quiz.js
+++ quiz.js
@@ -1,10 +1,12 @@
 var startButton = document.getElementById('start-btn')
 var nextButton = document.getElementById('next-btn')
+var endButton = document.getElementById("end-btn")
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
 var timeEl = document.getElementById('timer');
 var landPage = document.getElementById('landing')
+var scorePage = document.getElementById('scores')
 
 startButton.addEventListener('click', startGame)
 startButton.addEventListener('click', setTime)
@@ -13,35 +15,39 @@ nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
 })
+endButton.addEventListener('click', endGame)
 
-function hideLanding(){
+function hideLanding() {
   landPage.setAttribute("class", 'hide')
 }
 
 // var mainEl = document.getElementById("main");
-var secondsLeft = 10;
+var secondsLeft = 60;
+
 function setTime() {
   timeEl.removeAttribute("class", "hide")
   var timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft + " POINTS.";
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0 ) {
       clearInterval(timerInterval);
-      timeEl.innerHTML = "Game Over";   
+      timeEl.innerHTML = "Game Over";
       //form function   
     }
-  }, 1000);
+  }, 10000);
 
 }
 // setTime();
 
 
 function startGame() {
+
   startButton.setAttribute("class", 'hide')
   askQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
   questionContainerElement.removeAttribute("class", 'hide')
-  setNextQuestion()
+  setNextQuestion();
+  setTime();
 }
 
 function setNextQuestion() {
@@ -81,24 +87,104 @@ function selectAnswer(event) {
   //stop game
   if (askQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
+
   } else {
-    startButton.innerText = 'Stop the Clock!'
-    startButton.removeAttribute("class", 'hide')
+    endButton.innerText = 'Stop the Clock!'
+    endButton.removeAttribute("class", 'hide')
   }
 }
+
+function endGame() {
+  questionContainerElement.setAttribute("class", "hide")
+  scorePage.removeAttribute("class", "hide")
+  endButton.setAttribute("class", 'hide')
+}
+
+var scoreInput = document.querySelector('#score-text');
+var scoreForm = document.querySelector('#score-form');
+var scoreList = document.querySelector('#score-list');
+var scoreCountSpan = document.querySelector('#score-count');
+var scoreDiv = document.querySelector('scores');
+var scores = [];
+
+// init();
+
+
+  renderScores();
+
+var scores = [];
+
+function renderScores() {
+  
+  // Clear todoList element and update todoCountSpan
+  scoreList.innerHTML = "";
+  scoreCountSpan.textContent = scores.length;
+
+  // Render a new li for each score
+  for (var i = 0; i < scores.length; i++) {
+    var score = scores[i];
+
+    var li = document.createElement("li");
+    li.textContent = score;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.textContent = "Complete";
+
+    li.appendChild(button);
+    scoreList.appendChild(li);
+  }
+}
+
+// When form is submitted...
+scoreForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  var scoreText = scoreInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (scoreText === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  scores.push(scoreText);
+  scoreInput.value = "";
+
+  // Store updated todos in localStorage, re-render the list
+
+  renderScores();
+});
+
+// When a element inside of the todoList is clicked...
+scoreList.addEventListener("click", function (event) {
+  var element = event.target;
+
+  // If that element is a button...
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    scores.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+        renderScores();
+  }
+});
+
 
 function setStatusClass(element, correct) {
   clearStatusClass(element)
   if (correct) {
-    element.setAttribute("class", 'correct')
+    element.classList.add('correct')
   } else {
-    element.setAttribute("class", 'wrong')
+    element.classList.add('wrong')
   }
 }
 
 function clearStatusClass(element) {
-  element.removeAttribute("class", 'correct')
-  element.removeAttribute("class", 'wrong')
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+  element
 }
 
 var questions = [
@@ -108,7 +194,7 @@ var questions = [
       { text: 'Fast and the Furious character', correct: true },
       { text: 'Tree of Objects, On your Browser', correct: true },
       { text: 'Scary lady in leather', correct: true },
-      { text: 'Series of servers, location of a webpage', correct: true }
+      { text: 'Series of servers, location of a webpage', correct: false }
     ]
   },
   {
@@ -117,14 +203,14 @@ var questions = [
       { text: 'setElement', correct: true },
       { text: 'querySelector', correct: true },
       { text: 'getElementById', correct: true },
-      { text: 'pickThatOne', correct: true }
+      { text: 'pickThatOne', correct: false }
     ]
   },
   {
     question: 'What can execute a block of code, repeatedly?',
     answers: [
-      { text: 'a Loop', correct: false },
-      { text: 'an Array', correct: true },
+      { text: 'a Loop', correct: true },
+      { text: 'an Array', correct: false },
       { text: 'an Object', correct: false },
       { text: 'an Element', correct: false }
     ]
